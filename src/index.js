0ff22866c018485e958608e622277fd7d9aec878
RyanBegnaud <Ryanbegnaud13@gmail.com>
2020-10-27 14:20:52 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelectorAll("input")[1].addEventListener("click", function(event){
+    let desc = document.querySelectorAll("input")[0];
+    document.getElementById("list")
+    .innerText = desc.innerHTML;
+    event.preventDefault();
+  })
 });
