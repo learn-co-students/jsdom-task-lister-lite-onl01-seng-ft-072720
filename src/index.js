b7dbcb1eb94dbd7e1b3d8b82e826c5630b306458
NickSpangler <Spangleyman@mac.com>
2020-10-28 15:55:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let userInput = document.getElementById("new-task-description");
+    let taskList = document.getElementById("tasks");
+
+    if (userInput.value != "") {
+      const newLi = document.createElement("li");
+      const newTask = document.createTextNode(`${userInput.value} `);
+      const newButton = document.createElement("button");
+      const buttonText = document.createTextNode("X");
+      newLi.appendChild(newTask);
+      newButton.appendChild(buttonText);
+      newLi.appendChild(newButton)
+      taskList.appendChild(newLi);
+      userInput.value = "";
+      newButton.addEventListener("click", function(e) {
+        e.target.parentElement.remove()
+      })
+    };
+  }, false);
+
+
 });
