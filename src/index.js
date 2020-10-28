f002782fa1406b81caafeb32e7e666142bc51dd1
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-28 13:57:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+// document.addEventListener("keydown", function(e) {
+//   console.log(e.key);
+// });
+
+
+
+const button = document.querySelectorAll('input')[1];
+
+button.addEventListener('click', function(event)  {
+  let toDo = document.querySelector("#tasks");
+  let newLi = document.createElement("li")
+  let theText = document.getElementById("new-task-description").value
+  newLi.textContent = theText
+  toDo.appendChild(newLi)
+  event.preventDefault();
+});
+
+
+// function theClinking() {
+//   let toDo = document.querySelector("#tasks");
+//   let newLi = document.createElement("li")
+//   newLi.textContent = "More stuff"
+// toDo.appendChild(newLi)
+  // let newLi = document.createElement("li");
+ // newLi.innerText = 'Tony';
+  //document.body.div.div.ul.append(newLi);
+  
+//}
+//debugger
