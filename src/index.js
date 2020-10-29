79bb3c08b77b324447c927b543978abdc0378af2
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-29 16:00:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const formField = document.querySelector("#create-task-form");
   const todoList = document.querySelector("#tasks");
   const description = document.querySelector("#new-task-description");
-  const button = document.createElement("button");
+  const priority = document.querySelector("#priority");
+  // const button = document.createElement("button");
 
 
   formField.addEventListener("submit", function(event) {
@@ -11,11 +12,34 @@ document.addEventListener("DOMContentLoaded", () => {
     createTask();
   })
 
+  priority.addEventListener("change", function(event) {
+    console.log("selected")
+  })
+
   function createTask() {
     let li = document.createElement("li");
     li.innerHTML = description.value;
+    li.style.color = priorityColor(priority.value);
+    // debugger
     todoList.append(li);
     formField.reset();
+  }
+
+  function priorityColor(priority) {
+    switch(priority) {
+      case "high":
+        return "red";
+        break;
+      case "medium":
+        return "yellow";
+        break;
+      case "low":
+        return "green";
+        break;
+      default: 
+        return "black";
+        break;
+    }
   }
 
 });
