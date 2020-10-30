e9dc5564c13bd84607c84d592a10227a7b7d684f
EvanRPavone <evanrpavone@gmail.com>
2020-10-30 10:22:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const button = document.querySelectorAll('input')[1];
+  button.addEventListener('click', function (event) {
+    let toDo = document.getElementById('tasks');
+    let newLi = document.createElement('li')
+    let item = document.getElementById('new-task-description').value
+    newLi.textContent = item
+    toDo.appendChild(newLi)
+    event.preventDefault();
+  });
 });
