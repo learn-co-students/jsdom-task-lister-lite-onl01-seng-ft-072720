68a63286f7b51c6f2fdd68a1d1f24401523212a6
HuffDaddy0 <ty.huff24@gmail.com>
2020-10-30 13:43:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form")
+  const taskCard = document.querySelector("#tasks")
+
+
+
+
+  function getDescription(){
+   return form.querySelector("#new-task-description").value;
+  };
+  
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    let newCard = document.createElement("li")
+    taskCard.appendChild(newCard).innerHTML = getDescription();
+    form.reset()
+  });
 });
