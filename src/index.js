1b23aad6af89975fe4859a08569d5ab4cff11b03
KerriLynne <kerri.l.ogorman@gmail.com>
2020-11-02 14:15:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+//The deliverables require you to handle an event in a form based on clicking 
+//a submit button. You're going to need to listen for a submit event on the <form> element.
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+    
+  })
 });
